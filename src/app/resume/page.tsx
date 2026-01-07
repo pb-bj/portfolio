87a27f8a9bbc7a8a53936c8d0ebfedc2a45b4679
pb-bj <pranish2017bajracharya@gmail.com>
2026-01-07 22:54:56 +0545

    updated: resume

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,7 @@ export default function Resume() {
 	return (
 		<section className="section-container min-h-screen">
 			<div className="w-full h-[80vh] rounded-lg overflow-hidden border">
-				<iframe src="/resume/Pranish_Bajracharya.pdf" className="w-full h-full" title="Resume Preview" />
+				<iframe src="/resume/pranish-bajracharya-resume.pdf" className="w-full h-full" title="Resume Preview" />
 			</div>
 		</section>
 	);
